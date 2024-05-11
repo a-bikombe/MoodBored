49d87709b42b66cdf84da18b6263bebb6dbd015f
Arianna Bikombe <74719689+a-bikombe@users.noreply.github.com>
2024-05-11 10:09:16 -0400

    Implemented navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,10 +8,33 @@ import { LogNewMoodComponent } from './log-new-mood/log-new-mood.component';
 @Component({
     selector: 'app-root',
     standalone: true,
-    imports: [RouterOutlet, PastMoodsComponent, StatsComponent, JournalComponent, LogNewMoodComponent],
+    imports: [
+        RouterOutlet,
+        PastMoodsComponent,
+        StatsComponent,
+        JournalComponent,
+        LogNewMoodComponent,
+    ],
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss',
 })
 export class AppComponent {
     title = 'MoodBored';
+
+    // navlink: page link’s id
+    // page: page component’s id
+    showPage(navlink: string, page: string) {
+        let navLink = document.getElementById(navlink);
+        let pages = document.getElementsByClassName('page_component');
+
+        navLink?.addEventListener('click', function (e) {
+            for (let i = 0; i < pages.length; i++) {
+                if (pages.item(i)?.getAttribute('id') == page) {
+                    pages.item(i)?.setAttribute('style', 'display: flex;');
+                } else {
+                    pages.item(i)?.setAttribute('style', 'display: none;');
+                }
+            }
+        });
+    }
 }

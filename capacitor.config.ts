5d20e94f9220891dd213c603a84a178ad5d0c7ce
Arianna  Bikombe <abikombe@Edith.hsd1.nh.comcast.net>
2024-05-16 14:09:13 -0400

    iOS production

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+    appId: 'moodbored.app.01',
+    appName: 'MoodBored',
+    webDir: 'dist/mood-bored/browser'
+};
+
+export default config;
